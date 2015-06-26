cf43c586d216eedd1c283754b912485da47e6930
alkaloge <alkaloge@cern.ch>
2015-06-26 10:24:55 +0200

    latest code

--- DesyTauAnalyses/NTupleMaker/test/TreeProducerFromMiniAOD_V2.py
+++ DesyTauAnalyses/NTupleMaker/test/TreeProducerFromMiniAOD_V2.py
@@ -534,8 +534,8 @@ RecPhotonEtaMax = cms.untracked.double(10000.),
 RecPhotonHLTriggerMatching = cms.untracked.vstring(),
 RecPhotonNum = cms.untracked.int32(0),
 # electrons
-RecElectronPtMin = cms.untracked.double(10.),
-RecElectronEtaMax = cms.untracked.double(2.5),
+RecElectronPtMin = cms.untracked.double(8.),
+RecElectronEtaMax = cms.untracked.double(2.6),
 RecElectronHLTriggerMatching = cms.untracked.vstring(
 'HLT_Ele27_eta2p1_WP85_Gsf_v.*:hltEle27WP85GsfTrackIsoFilter',
 'HLT_Ele32_eta2p1_WP85_Gsf_v.*:hltEle32WP85GsfTrackIsoFilter',
@@ -663,3 +663,60 @@ process.TFileService = cms.Service("TFileService",
 
 
 
+
+def customise_for_gc(process):
+	import FWCore.ParameterSet.Config as cms
+	from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper
+
+	try:
+		maxevents = __MAX_EVENTS__
+		process.maxEvents = cms.untracked.PSet(
+			input = cms.untracked.int32(max(-1, maxevents))
+		)
+	except:
+		pass
+
+	# Dataset related setup
+	try:
+		primaryFiles = [__FILE_NAMES__]
+		process.source = cms.Source('PoolSource',
+			skipEvents = cms.untracked.uint32(__SKIP_EVENTS__),
+			fileNames = cms.untracked.vstring(primaryFiles)
+		)
+		try:
+			secondaryFiles = [__FILE_NAMES2__]
+			process.source.secondaryFileNames = cms.untracked.vstring(secondaryFiles)
+		except:
+			pass
+		try:
+			lumirange = [__LUMI_RANGE__]
+			if len(lumirange) > 0:
+				process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange(lumirange)
+				process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+		except:
+			pass
+	except:
+		pass
+
+	if hasattr(process, 'RandomNumberGeneratorService'):
+		randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)
+		randSvc.populate()
+
+	process.AdaptorConfig = cms.Service('AdaptorConfig',
+		enable = cms.untracked.bool(True),
+		stats = cms.untracked.bool(True),
+	)
+
+	# Generator related setup
+	try:
+		if hasattr(process, 'generator') and process.source.type_() != 'PoolSource':
+			process.source.firstLuminosityBlock = cms.untracked.uint32(1 + __MY_JOBID__)
+			print 'Generator random seed:', process.RandomNumberGeneratorService.generator.initialSeed
+	except:
+		pass
+
+	return (process)
+
+process = customise_for_gc(process)
+
+# grid-control: https://ekptrac.physik.uni-karlsruhe.de/trac/grid-control
